88cd0285ffff984a0c66b5e1d4bd01a91871bb5b
wlanboy <me@wlanboy.com>
2019-03-31 20:00:46 +0200

    Added basic Spring application and properties.

---
+++ src/main/java/com/wlanboy/cloudtask/CloudtaskApplication.java
@@ -0,0 +1,13 @@
+package com.wlanboy.cloudtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudtaskApplication.class, args);
+	}
+
+}
